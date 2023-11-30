1cb209509ee938c7b20d09997b478314c49a56f4
사공민수 <149641823+sagongminsu@users.noreply.github.com>
2023-11-30 18:00:29 +0900

    Paddle
    Script
    1.Keyboard1
     강의 자료참조
    2.Keyboard
     해석영상 참조
    3.Keyboard
     좌표값을 추가하여 작성
    4.Mouse
     (미완성)

---
+++ Assets/Script/Keyboard/Paddle.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public bool Palyer1;
+    public float speed;
+    public Rigidbody2D rigidbody;
+
+    public KeyCode Right;
+    public KeyCode Left;
+
+    private float movement;
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        movement = 0f;
+        if (Input.GetKey(Right)) { movement += 1f; }
+        if (Input.GetKey(Left)) { movement -= 1f; }
+        rigidbody.velocity = new Vector2( movement * speed,0);
+    }
+
+    public void Reset()
+    {
+        rigidbody.velocity = Vector3.zero;
+        transform.position = startPosition;
+    }
+}
