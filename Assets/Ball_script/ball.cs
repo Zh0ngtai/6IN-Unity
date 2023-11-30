4c3aa095f943b7d62c724a30763f57fcc8b96418
ebanlee1204 <ebanlee1204@gmail.com>
2023-11-30 19:39:53 +0900

    Ball Part

---
+++ Assets/Ball_script/ball.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    public Rigidbody2D rb;
+    float X, Y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.AddForce(Random.insideUnitSphere * speed, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+        
+    //}
+}
