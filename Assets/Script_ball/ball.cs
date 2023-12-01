efe1d4416476737c24903c6bffea4bbcb8b983b4
ebanlee1204 <ebanlee1204@gmail.com>
2023-12-01 17:26:35 +0900

    Add ball dead

--- Assets/Script_ball/ball.cs
+++ Assets/Script_ball/ball.cs
@@ -20,6 +20,10 @@ public class ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 
     //private void OnCollisionEnter2D(Collision2D collision)
