9fda65bf7446e0e9954faefa5de4729d88eab712
사공민수 <149641823+sagongminsu@users.noreply.github.com>
2023-12-03 19:08:51 +0900

    [add] item 0.1
    7가지중에서
    1번 Paddle의 크기증가 구현
    ----
    추가 사항
    상세설명
    Brick
    1-1:c#에 90프로(임의)의 확률로 아이템이 생성
    ----
    1-2:오브젝트에서 Prefabs로 정렬을 사용하여 추가 구성시 변경가능
    1-2-1
    사용방법
    게임오브젝트에서  Element에 드레그 엔 드랍을 하여 추가가능
    추가시에 아이템 c#의 구현 및 추가 이미지 필요

--- Assets/Scripts/Script_brick/Brick.cs
+++ Assets/Scripts/Script_brick/Brick.cs
@@ -6,17 +6,36 @@ public class Brick : MonoBehaviour
 {
     int brickLife = 1;
 
-    // Update is called once per frame
+    // 아이템 프리팹들을 인스펙터에서 설정할 수 있도록 설정
+    public GameObject[] itemPrefabs;
+
+    
     void FixedUpdate()
     {
         if (brickLife == 0)
         {
+            // 90%의 확률로 아이템 생성
+            if (Random.Range(0f, 1f) <= 0.9f)
+            {
+                SpawnRandomItem();
+            }
+
             Destroy(gameObject);
         }
     }
 
-    private void OnCollisionEnter2D (Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         brickLife--;
+    }
+
+    void SpawnRandomItem()
+    {
+        // 아이템 프리팹 배열에서 무작위로 하나를 선택
+        int randomIndex = Random.Range(0, itemPrefabs.Length);
+        GameObject randomItemPrefab = itemPrefabs[randomIndex];
+
+        // 선택된 아이템을 현재 벽돌의 위치에 생성
+        Instantiate(randomItemPrefab, transform.position, Quaternion.identity);
     }
 }
